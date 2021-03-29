ebdee94c1249db556e20c26049699fc10acc438e
galli <54688233+SJRNoodles@users.noreply.github.com>
2021-03-29 17:53:14 -0500

    Create atomic.js

---
+++ atomic.js
@@ -0,0 +1,12 @@
+function drawText(a,t) {
+document.write("<p style='"+a+"'>"+t+"</p>");
+}
+function drawHeader(h,a,t) {
+document.write("<h"+h+" style='"+a+"'>"+t+"</h"+h+">");
+}
+function insertBreak(){
+document.write("<br>");
+}
+function insertHeadLine(){
+document.write("<hr>");
+}
