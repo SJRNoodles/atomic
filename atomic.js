b9ef49eaca8aa7f5f3ca6af5c563938cddb948f2
galli <54688233+SJRNoodles@users.noreply.github.com>
2021-03-29 18:10:46 -0500

    Update atomic.js

--- atomic.js
+++ atomic.js
@@ -1,6 +1,9 @@
 function drawText(a,t) {
 document.write("<p style='"+a+"'>"+t+"</p>");
 }
+function drawLink(l,a,t) {
+document.write("<a href='"+l+"'style='"+a+"'>"+t+"</a>");
+}
 function drawHeader(h,a,t) {
 document.write("<h"+h+" style='"+a+"'>"+t+"</h"+h+">");
 }
@@ -9,4 +12,10 @@ document.write("<br>");
 }
 function insertHeadLine(){
 document.write("<hr>");
+}
+function insertCodeSnip(code){
+document.write("<code style='color:#ffa;background-color:#260C0C;'>"+code+"</code>");
+}
+function Bg(color){
+document.write("<style>body{background-color:"+color+";}</style>");
 }
