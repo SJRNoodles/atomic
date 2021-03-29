be6c85523ce3d3638a57b05cdad17f215dd9c6d9
galli <54688233+SJRNoodles@users.noreply.github.com>
2021-03-29 18:31:01 -0500

    Update atomic.js

--- atomic.js
+++ atomic.js
@@ -1,20 +1,23 @@
-function drawText(a,t) {
+function dTex(a,t) {
 document.write("<p style='"+a+"'>"+t+"</p>");
 }
-function drawLink(l,a,t) {
+function dLink(l,a,t) {
 document.write("<a href='"+l+"'style='"+a+"'>"+t+"</a>");
 }
-function drawHeader(h,a,t) {
+function dHead(h,a,t) {
 document.write("<h"+h+" style='"+a+"'>"+t+"</h"+h+">");
 }
-function insertBreak(){
+function br(){
 document.write("<br>");
 }
-function insertHeadLine(){
+function hr(){
 document.write("<hr>");
 }
-function insertCodeSnip(code){
+function dCode(code){
 document.write("<code style='color:#ffa;background-color:#260C0C;'>"+code+"</code>");
+}
+function dImg(img){
+document.write("<img src='"+img+"'>");
 }
 function Bg(color){
 document.write("<style>body{background-color:"+color+";}</style>");
